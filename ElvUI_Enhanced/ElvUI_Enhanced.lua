6c9a70234528d9fcd6c078fc557dd2be0f0b142a
Crum <stschleyer89@gmail.com>
2017-09-27 20:20:23 -0500

    update and cleanup

--- ElvUI_Enhanced/ElvUI_Enhanced.lua
+++ ElvUI_Enhanced/ElvUI_Enhanced.lua
@@ -1,6 +1,8 @@
 local E, L, V, P, G = unpack(ElvUI)
 local addon = E:NewModule("ElvUI_Enhanced")
 
+local addonName = ...
+
 local LEP = LibStub("LibElvUIPlugin-1.0")
 
 function addon:Initialize()
@@ -10,7 +12,15 @@ function addon:Initialize()
 		print(format(L["ENH_LOGIN_MSG"], E["media"].hexvaluecolor, addon.version))
 	end
 
-	LEP:RegisterPlugin("ElvUI_Enhanced", self.GetOptions)
+	-- DBConversions
+	if E.db.enhanced.tooltip.progressInfo.tiers.TotC then
+		E.db.enhanced.tooltip.progressInfo.tiers.ToC = true
+	end
+	if E.db.enhanced.tooltip.progressInfo.tiers.TotC ~= nil then
+		E.db.enhanced.tooltip.progressInfo.tiers.TotC = nil
+	end
+
+	LEP:RegisterPlugin(addonName, self.GetOptions)
 
 	if E.db.general.showQuestLevel then
 		E.db.enhanced.general.showQuestLevel = true
