1dbf15663c8cf976dd559307f1fa4feacdb4d0a2
Bunny67 <zolov67@yandex.ru>
2018-01-03 04:29:45 +0300

    Revert "update skins folder"
    This reverts commit b8a2c68a389b8b2c2445e7c25bec026b6eb83dbb.

--- ElvUI_Enhanced/modules/Skins/Blizzard/TradeSkill.lua
+++
@@ -1,8 +0,0 @@
-local E, L, V, P, G = unpack(ElvUI)
-local S = E:GetModule("Skins")
-
-local function LoadSkin()
-	S:SearchBoxTemplate(TradeSkillFrameEditBox)
-end
-
-S:AddCallbackForAddon("Blizzard_TradeSkillUI", "Enhanced_TradeSkillUI", LoadSkin)
