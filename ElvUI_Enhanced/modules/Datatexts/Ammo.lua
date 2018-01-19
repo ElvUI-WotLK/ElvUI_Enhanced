c5f15d356778fad001758107bca89943a76ce4d7
Apollyonn <jellefenxd@gmail.com>
2018-01-19 15:54:39 +0200

    update

--- ElvUI_Enhanced/modules/Datatexts/Ammo.lua
+++ ElvUI_Enhanced/modules/Datatexts/Ammo.lua
@@ -32,58 +32,58 @@ end
 
 local Ammo = {
 	-- Arrow
-	["2512"] = {ID = 2512},		-- Rough Arrow
-	["2515"] = {ID = 2515},		-- Sharp Arrow
-	["3030"] = {ID = 3030},		-- Razor Arrow
-	["3464"] = {ID = 3464},		-- Feathered Arrow
-	["9399"] = {ID = 9399},		-- Precision Arrow
-	["11285"] = {ID = 11285},	-- Jagged Arrow
-	["19316"] = {ID = 19316},	-- Ice Threaded Arrow
-	["18042"] = {ID = 18042},	-- Thorium Headed Arrow
-	["12654"] = {ID = 12654},	-- Doomshot
-	["28053"] = {ID = 28053},	-- Wicked Arrow
-	["24417"] = {ID = 24417},	-- Scout's Arrow
-	["28056"] = {ID = 28056},	-- Blackflight Arrow
-	["30611"] = {ID = 30611},	-- Halaani Razorshaft
-	["56176"] = {ID = 56176},	-- Warden's Arrow
-	["33803"] = {ID = 33803},	-- Adamantite Stinger
-	["34581"] = {ID = 34581},	-- Mysterious Arrow
-	["31737"] = {ID = 31737},	-- Timeless Arrow
-	["30319"] = {ID = 30319},	-- Nether Spike
-	["41165"] = {ID = 41165},	-- Saronite Razorheads
-	["41586"] = {ID = 41586},	-- Terrorshaft Arrow
-	["52021"] = {ID = 52021},	-- Iceblade Arrow
+	"2512",		-- Rough Arrow
+	"2515",		-- Sharp Arrow
+	"3030",		-- Razor Arrow
+	"3464",		-- Feathered Arrow
+	"9399",		-- Precision Arrow
+	"11285",	-- Jagged Arrow
+	"12654",	-- Doomshot
+	"18042",	-- Thorium Headed Arrow
+	"19316",	-- Ice Threaded Arrow
+	"24417",	-- Scout's Arrow
+	"28053",	-- Wicked Arrow
+	"28056",	-- Blackflight Arrow
+	"30319",	-- Nether Spike
+	"30611",	-- Halaani Razorshaft
+	"31737",	-- Timeless Arrow
+	"33803",	-- Adamantite Stinger
+	"34581",	-- Mysterious Arrow
+	"41165",	-- Saronite Razorheads
+	"41586",	-- Terrorshaft Arrow
+	"52021",	-- Iceblade Arrow
+	"56176",	-- Warden's Arrow
 	-- Bullet
-	["2516"] = {ID = 2516},		-- Light Shot
-	["4960"] = {ID = 4960},		-- Flash Pellet
-	["8067"] = {ID = 8067},		-- Crafted Light Shot
-	["2519"] = {ID = 2519},		-- Heavy Shot
-	["5568"] = {ID = 5568},		-- Smooth Pebble
-	["8068"] = {ID = 8068},		-- Crafted Heavy Shot
-	["3033"] = {ID = 3033},		-- Solid Shot
-	["8069"] = {ID = 8069},		-- Crafted Solid Shot
-	["3465"] = {ID = 3465},		-- Exploding Shot
-	["10512"] = {ID = 10512},	-- Hi-Impact Mithril Slugs
-	["11284"] = {ID = 11284},	-- Accurate Slugs
-	["10513"] = {ID = 10513},	-- Mithril Gyro-Shot
-	["19317"] = {ID = 19317},	-- Ice Threaded Bullet
-	["15997"] = {ID = 15997},	-- Thorium Shells
-	["11630"] = {ID = 11630},	-- Rockshard Pellets
-	["13377"] = {ID = 13377},	-- Miniature Cannon Balls
-	["28060"] = {ID = 28060},	-- Impact Shot
-	["23772"] = {ID = 23772},	-- Fel Iron Shells
-	["28061"] = {ID = 28061},	-- Ironbite Shell
-	["30612"] = {ID = 30612},	-- Halaani Grimshot
-	["32883"] = {ID = 32883},	-- Felbane Slugs
-	["32882"] = {ID = 32882},	-- Hellfire Shot
-	["23773"] = {ID = 23773},	-- Adamantite Shells
-	["34582"] = {ID = 34582},	-- Mysterious Shell
-	["31735"] = {ID = 31735},	-- Timeless Shell
-	["41164"] = {ID = 41164},	-- Mammoth Cutters
-	["41584"] = {ID = 41584},	-- Frostbite Bullets
-	["52020"] = {ID = 52020},	-- Shatter Rounds
+	"2516",		-- Light Shot
+	"2519",		-- Heavy Shot
+	"3033",		-- Solid Shot
+	"3465",		-- Exploding Shot
+	"4960",		-- Flash Pellet
+	"5568",		-- Smooth Pebble
+	"8067",		-- Crafted Light Shot
+	"8068",		-- Crafted Heavy Shot
+	"8069",		-- Crafted Solid Shot
+	"10512",	-- Hi-Impact Mithril Slugs
+	"10513",	-- Mithril Gyro-Shot
+	"11284",	-- Accurate Slugs
+	"11630",	-- Rockshard Pellets
+	"13377",	-- Miniature Cannon Balls
+	"15997",	-- Thorium Shells
+	"19317",	-- Ice Threaded Bullet
+	"23772",	-- Fel Iron Shells
+	"23773",	-- Adamantite Shells
+	"28060",	-- Impact Shot
+	"28061",	-- Ironbite Shell
+	"30612",	-- Halaani Grimshot
+	"32882",	-- Hellfire Shot
+	"32883",	-- Felbane Slugs
+	"31735",	-- Timeless Shell
+	"34582",	-- Mysterious Shell
+	"41584",	-- Frostbite Bullets
+	"41164",	-- Mammoth Cutters
+	"52020",	-- Shatter Rounds
 	-- Soul Shard
-	["6265"] = {ID = 6265},		-- Soul Shard
+	"6265",		-- Soul Shard
 }
 
 local function OnEvent(self)
@@ -122,8 +122,8 @@ local function OnEnter(self)
 	local count
 	local _, name, quality, subclass, texture
 	for _, info in pairs(Ammo) do
-		name, _, quality, _, _, _, subclass, _, _, texture = GetItemInfo(info.ID)
-		count = GetItemCount(info.ID)
+		name, _, quality, _, _, _, subclass, _, _, texture = GetItemInfo(info)
+		count = GetItemCount(info)
 		if quality then
 			r, g, b = GetItemQualityColor(quality)
 		else
