ec4aa3ed005fcd63bdfc318dd5e836defdc157cd
Apollyonn <jellefenxd@gmail.com>
2018-01-19 16:50:20 +0200

    update

--- ElvUI_Enhanced/modules/Datatexts/Ammo.lua
+++ ElvUI_Enhanced/modules/Datatexts/Ammo.lua
@@ -30,62 +30,6 @@ local function ColorizeSettingName(settingName)
 	return format("|cffff8000%s|r", settingName)
 end
 
-local Ammo = {
-	-- Arrow
-	"2512",		-- Rough Arrow
-	"2515",		-- Sharp Arrow
-	"3030",		-- Razor Arrow
-	"3464",		-- Feathered Arrow
-	"9399",		-- Precision Arrow
-	"11285",	-- Jagged Arrow
-	"12654",	-- Doomshot
-	"18042",	-- Thorium Headed Arrow
-	"19316",	-- Ice Threaded Arrow
-	"24417",	-- Scout's Arrow
-	"28053",	-- Wicked Arrow
-	"28056",	-- Blackflight Arrow
-	"30319",	-- Nether Spike
-	"30611",	-- Halaani Razorshaft
-	"31737",	-- Timeless Arrow
-	"33803",	-- Adamantite Stinger
-	"34581",	-- Mysterious Arrow
-	"41165",	-- Saronite Razorheads
-	"41586",	-- Terrorshaft Arrow
-	"52021",	-- Iceblade Arrow
-	"56176",	-- Warden's Arrow
-	-- Bullet
-	"2516",		-- Light Shot
-	"2519",		-- Heavy Shot
-	"3033",		-- Solid Shot
-	"3465",		-- Exploding Shot
-	"4960",		-- Flash Pellet
-	"5568",		-- Smooth Pebble
-	"8067",		-- Crafted Light Shot
-	"8068",		-- Crafted Heavy Shot
-	"8069",		-- Crafted Solid Shot
-	"10512",	-- Hi-Impact Mithril Slugs
-	"10513",	-- Mithril Gyro-Shot
-	"11284",	-- Accurate Slugs
-	"11630",	-- Rockshard Pellets
-	"13377",	-- Miniature Cannon Balls
-	"15997",	-- Thorium Shells
-	"19317",	-- Ice Threaded Bullet
-	"23772",	-- Fel Iron Shells
-	"23773",	-- Adamantite Shells
-	"28060",	-- Impact Shot
-	"28061",	-- Ironbite Shell
-	"30612",	-- Halaani Grimshot
-	"32882",	-- Hellfire Shot
-	"32883",	-- Felbane Slugs
-	"31735",	-- Timeless Shell
-	"34582",	-- Mysterious Shell
-	"41584",	-- Frostbite Bullets
-	"41164",	-- Mammoth Cutters
-	"52020",	-- Shatter Rounds
-	-- Soul Shard
-	"6265",		-- Soul Shard
-}
-
 local function OnEvent(self)
 	local name, count, link
 	if E.myclass == "WARLOCK" then
@@ -112,6 +56,7 @@ end
 
 local function OnEnter(self)
 	DT:SetupTooltip(self)
+
 	if E.myclass == "WARLOCK" then
 		DT.tooltip:AddLine(GetItemInfo(6265))
 	else
@@ -119,27 +64,35 @@ local function OnEnter(self)
 	end
 
 	local r, g, b
-	local count
-	local _, name, quality, subclass, texture
-	for _, info in pairs(Ammo) do
-		name, _, quality, _, _, _, subclass, _, _, texture = GetItemInfo(info)
-		count = GetItemCount(info)
-		if quality then
-			r, g, b = GetItemQualityColor(quality)
-		else
-			r, g, b = 1, 1, 1
-		end
+	local item, link, count
+	local _, name, quality, subclass, equipLoc, texture
 
-		if name and (count > 0) then
-			DT.tooltip:AddDoubleLine(join("", format(iconString, texture), " ", name), count, r, g, b)
+	for i = 0, 4 do
+		for j = 1, GetContainerNumSlots(i) do
+			item = GetContainerItemID(i, j)
+			if item then
+				link = GetContainerItemLink(i, j)
+				name, _, quality, _, _, _, subclass, _, equipLoc, texture = GetItemInfo(link)
+				count = GetItemCount(link)
+
+				if quality then
+					r, g, b = GetItemQualityColor(quality)
+				else
+					r, g, b = 1, 1, 1
+				end
+
+				if equipLoc == "INVTYPE_AMMO" then
+					DT.tooltip:AddDoubleLine(join("", format(iconString, texture), " ", name), count, r, g, b)
+				end
+			end
 		end
 	end
 
 	local free, total, used = 0, 0, 0
 	for i = 1, NUM_BAG_SLOTS do
-		local bagLink = GetInventoryItemLink("player", ContainerIDToInventoryID(i))
-		if bagLink then
-			name, _, quality, _, _, _, subclass, _, _, texture = GetItemInfo(bagLink)
+		link = GetInventoryItemLink("player", ContainerIDToInventoryID(i))
+		if link then
+			name, _, quality, _, _, _, subclass, _, _, texture = GetItemInfo(link)
 			if quality then
 				r, g, b = GetItemQualityColor(quality)
 			else
