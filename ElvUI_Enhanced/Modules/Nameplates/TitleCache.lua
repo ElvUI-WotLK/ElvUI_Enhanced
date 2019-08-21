759992cf76cec540394d0f48b99cb063096934cd
bunny67 <zolov6767@gmail.com>
2019-08-22 01:12:55 +0300

    test

--- ElvUI_Enhanced/Modules/Nameplates/TitleCache.lua
+++
@@ -1,9 +0,0 @@
-local E, L, V, P, G = unpack(ElvUI)
-local ENP = E:GetModule("Enhanced_NamePlates")
-local NP = E:GetModule("NamePlates")
-
-
-local function ConfigureElement_Title(frame)
-
-end
-
--- ElvUI_Enhanced/Modules/Nameplates/nameplates.lua
+++ ElvUI_Enhanced/Modules/Nameplates/nameplates.lua
@@ -3,6 +3,7 @@ local ENP = E:NewModule("Enhanced_NamePlates", "AceHook-3.0", "AceEvent-3.0")
 local NP = E:GetModule("NamePlates")
 
 local pairs = pairs
+local tinsert = table.insert
 
 local UnitClass = UnitClass
 local UnitIsPlayer = UnitIsPlayer
@@ -35,13 +36,49 @@ function ENP:UPDATE_MOUSEOVER_UNIT()
 			if not guildName then return end
 
 			if not guildMap[guildName] then
-				table.insert(EnhancedDB.GuildList, guildName)
+				tinsert(EnhancedDB.GuildList, guildName)
 				guildMap[guildName] = #EnhancedDB.GuildList
 			end
 
 			if EnhancedDB.UnitTitle[name] ~= guildMap[guildName] then
 				EnhancedDB.UnitTitle[name] = guildMap[guildName]
 			end
+		end
+	end
+end
+
+local function UnitClassHook(self, frame, unitType)
+	if unitType == "FRIENDLY_PLAYER" then
+		local unitName = frame.UnitName
+		local unit = self[unitType][unitName]
+		if unit then
+			local _, class = UnitClass(unit)
+			if class then
+				return class
+			end
+		elseif EnhancedDB.UnitClass[unitName] then
+			return CLASS_SORT_ORDER[EnhancedDB.UnitClass[unitName]]
+		else
+			return NP:GetUnitClassByGUID(frame)
+		end
+	elseif unitType == "ENEMY_PLAYER" then
+		local r, g, b = self:RoundColors(frame.oldHealthBar:GetStatusBarColor())
+		for class in pairs(RAID_CLASS_COLORS) do -- ENEMY_PLAYER
+			if RAID_CLASS_COLORS[class].r == r and RAID_CLASS_COLORS[class].g == g and RAID_CLASS_COLORS[class].b == b then
+				return class
+			end
+		end
+	end
+end
+
+function ENP:ClassCache()
+	if E.db.enhanced.nameplates.classCache then
+		if not self:IsHooked(NP, "UnitClass") then
+			self:RawHook(NP, "UnitClass", UnitClassHook)
+		end
+	else
+		if self:IsHooked(NP, "UnitClass") then
+			self:Unhook(NP, "UnitClass")
 		end
 	end
 end
@@ -91,7 +128,6 @@ function ENP:Initialize()
 
 	if E.db.enhanced.nameplates.titleCache or E.db.enhanced.nameplates.classCache then
 		self:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
-
 	elseif not E.db.enhanced.nameplates.titleCache and not E.db.enhanced.nameplates.classCache then
 		self:UnregisterEvent("UPDATE_MOUSEOVER_UNIT")
 	end
