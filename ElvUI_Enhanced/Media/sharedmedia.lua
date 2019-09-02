05fa466a8f9d55f6e1b7a1c2eded7e37f932011e
Apollyonn <jellefenxd@gmail.com>
2019-09-03 00:23:46 +0300

    rename

--- ElvUI_Enhanced/Media/sharedmedia.lua
+++
@@ -1,3 +0,0 @@
-local LSM = LibStub("LibSharedMedia-3.0")
-
-LSM:Register("font", "TukUI Unitframes", [[Interface\Addons\ElvUI_Enhanced\Media\fonts\uf_font.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western);
---
+++ ElvUI_Enhanced/Media/sharedmedia_.lua
@@ -0,0 +1,4 @@
+local E = unpack(ElvUI)
+local LSM = E.Libs.LSM
+
+LSM:Register("font", "TukUI Unitframes", [[Interface\Addons\ElvUI_Enhanced\Media\fonts\uf_font.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
