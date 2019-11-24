5412dceee66bef2e1528aeed5c2db151ecef2c85
Pinya <800pin.ru@gmail.com>
2019-11-24 18:25:24 +0300

    Update AddonsCompatibility.lua

--- ElvUI_Enhanced/Modules/Misc/AddonsCompatibility.lua
+++ ElvUI_Enhanced/Modules/Misc/AddonsCompatibility.lua
@@ -2,11 +2,15 @@ local E, L, V, P, G = unpack(ElvUI)
 local AC = E:NewModule("Enhanced_AddonsCompat", "AceEvent-3.0")
 
 local pairs, ipairs = pairs, ipairs
+local type = type
 local tinsert, tremove = table.insert, table.remove
 
+local GetAddOnInfo = GetAddOnInfo
+local GetNumAddOns = GetNumAddOns
 local IsAddOnLoadOnDemand = IsAddOnLoadOnDemand
 local IsAddOnLoaded = IsAddOnLoaded
-local LoadAddOn = LoadAddOn
+
+local externalFixes = {}
 
 local addonFixes = {
 	-- Cromulent 1.5.2
@@ -41,7 +45,9 @@ local addonFixes = {
 		if DT.RegisteredDataTexts and DT.RegisteredDataTexts["Time"] then
 			DT.RegisteredDataTexts["Time"].onClick = function(_, btn)
 				if btn == "RightButton" then
-					if not IsAddOnLoaded("Blizzard_TimeManager") then LoadAddOn("Blizzard_TimeManager") end
+					if not IsAddOnLoaded("Blizzard_TimeManager") then
+						LoadAddOn("Blizzard_TimeManager")
+					end
 					TimeManagerClockButton_OnClick(TimeManagerClockButton)
 				elseif GroupCalendar and GroupCalendar.ToggleCalendarDisplay then
 					GroupCalendar.ToggleCalendarDisplay()
@@ -115,6 +121,7 @@ local addonFixes = {
 	["DrDamage"] = function()
 		if not E.private.actionbar.enable then return end
 
+		local HasAction = HasAction
 		local SecureButton_GetEffectiveButton = SecureButton_GetEffectiveButton
 		local SecureButton_GetModifiedAttribute = SecureButton_GetModifiedAttribute
 
@@ -150,31 +157,65 @@ local addonFixes = {
 	end,
 }
 
-function AC:AddAddon(addon)
-	if not addon then return end
+function AC:AddAddon(addon, func)
+	if type(addon) ~= "string" then
+		error(string.format("bad argument #1 to 'AddAddon' (string expected, got %s)", addon ~= nil and type(addon) or "no value"), 2)
+	elseif func and type(func) ~= "function" then
+		error(string.format("bad argument #2 to 'AddAddon' (function expected, got %s)", func ~= nil and type(func) or "no value"), 2)
+	end
+
+	if not self.initialized then
+		self.preinitList = self.preinitList or {}
+		self.preinitList[addon] = func
+		return
+	end
+
+	if not self.addonList[addon] then return end
 
 	if IsAddOnLoaded(addon) then
-		self:ApplyFix(addon)
+		self:ApplyFix(addon, func)
 	elseif IsAddOnLoadOnDemand(addon) then
+		if not addonFixes[addon] then
+			externalFixes[addon] = externalFixes[addon] or {}
+			tinsert(externalFixes[addon], func)
+		end
+
 		tinsert(self.addonQueue, addon)
 		self:RegisterEvent("ADDON_LOADED")
 	end
 end
 
-function AC:ApplyFix(addon, onDemandID)
-	addonFixes[addon]()
+function AC:ApplyFix(addon, func)
+	if func then
+		func()
 
-	if onDemandID then
-		tremove(self.addonQueue, onDemandID)
+		if addonFixes[addon] then
+			addonFixes[addon] = nil
+		end
+	else
+		if addonFixes[addon] then
+			addonFixes[addon]()
+			addonFixes[addon] = nil
+		end
+
+		if externalFixes[addon] then
+			for i = 1, #externalFixes[addon] do
+				externalFixes[addon][i]()
+			end
+
+			externalFixes[addon] = nil
+		end
 	end
 end
 
 function AC:ADDON_LOADED(_, addonName)
-	if not addonFixes[addonName] then return end
+	if not (addonFixes[addonName] or externalFixes[addonName]) then return end
 
 	for i, addon in ipairs(self.addonQueue) do
 		if addon == addonName then
-			self:ApplyFix(addon, i)
+			self:ApplyFix(addon)
+
+			tremove(self.addonQueue, i)
 
 			if #self.addonQueue == 0 then
 				self:UnregisterEvent("ADDON_LOADED")
@@ -187,10 +228,27 @@ end
 
 function AC:Initialize()
 	self.addonQueue = {}
+	self.addonList = {}
+
+	for i = 1, GetNumAddOns() do
+		local name, _, _, enabled = GetAddOnInfo(i)
+
+		if enabled or IsAddOnLoadOnDemand(i) then
+			self.addonList[name] = true
+		end
+	end
 
 	for addon, func in pairs(addonFixes) do
 		self:AddAddon(addon, func)
 	end
+
+	if self.preinitList then
+		for addon, func in pairs(self.preinitList) do
+			self:AddAddon(addon, func)
+		end
+	end
+
+	self.initialized = true
 end
 
 local function InitializeCallback()
