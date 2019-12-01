363043abddd98b2f294d083ea55bd69ec93c02c3
Pinya <800pin.ru@gmail.com>
2019-12-01 18:15:21 +0300

    refactor KeyPressAnimation

--- ElvUI_Enhanced/Modules/Actionbars/KeyPressAnimation.lua
+++ ElvUI_Enhanced/Modules/Actionbars/KeyPressAnimation.lua
@@ -3,76 +3,86 @@ local KPA = E:NewModule("Enhanced_KeyPressAnimation")
 local LAB = E.Libs.LAB
 
 local ipairs = ipairs
-local pairs = pairs
+local tinsert, tremove = table.insert, table.remove
 
-local animationsCount = 5
-local animations = {}
+local CreateFrame = CreateFrame
 
-for i = 1, animationsCount do
-	local frame = CreateFrame("Frame")
+local framePool = {}
+
+local function OnFinished(self)
+	tinsert(framePool, self.parent)
+end
+
+local function CreateAnimationFrame()
+	local db = E.db.enhanced.actionbar.keyPressAnimation
+
+	local frame = CreateFrame("Frame", "Enhanced_KeyPressAnimation"..(#KPA.frames + 1), UIParent)
 
 	local texture = frame:CreateTexture()
 	texture:SetTexture([[Interface\Cooldown\star4]])
 	texture:SetAlpha(0)
 	texture:SetAllPoints()
 	texture:SetBlendMode("ADD")
+	texture:SetVertexColor(db.color.r, db.color.g, db.color.b)
 	frame.texture = texture
 
 	local animationGroup = texture:CreateAnimationGroup()
+	animationGroup:SetScript("OnFinished", OnFinished)
+	animationGroup.parent = frame
+	frame.animationGroup = animationGroup
 
 	local alpha1 = animationGroup:CreateAnimation("Alpha")
 	alpha1:SetChange(1)
 	alpha1:SetDuration(0)
 	alpha1:SetOrder(1)
+	frame.alpha1 = alpha1
 
 	local scale1 = animationGroup:CreateAnimation("Scale")
 	scale1:SetScale(1.0, 1.0)
 	scale1:SetDuration(0)
 	scale1:SetOrder(1)
+	frame.scale1 = scale1
 
 	local scale2 = animationGroup:CreateAnimation("Scale")
-	scale2:SetScale(1.5, 1.5)
+	scale2:SetScale(db.scale, db.scale)
 	scale2:SetDuration(0.2)
 	scale2:SetOrder(2)
 	frame.scale2 = scale2
 
 	local rotation = animationGroup:CreateAnimation("Rotation")
-	rotation:SetDegrees(90)
+	rotation:SetDegrees(db.rotation)
 	rotation:SetDuration(0.2)
 	rotation:SetOrder(2)
 	frame.rotation = rotation
 
-	animations[i] = {frame = frame, animationGroup = animationGroup}
+	tinsert(KPA.frames, frame)
+
+	return frame
 end
 
-local animationNum = 1
-local function animate(button)
-	if not button:IsVisible() or button:GetParent():GetAlpha() == 0 then
-		return
-	end
+local function StartAnimation(button)
+	if not button:IsVisible() or button:GetParent():GetAlpha() == 0 then return end
 
-	local animation = animations[animationNum]
-	local frame = animation.frame
-	local animationGroup = animation.animationGroup
+	local frame = KPA:GetFreeAnimationFrame()
+	local animationGroup = frame.animationGroup
 
 	frame:SetFrameStrata(button:GetFrameStrata())
 	frame:SetFrameLevel(button:GetFrameLevel() + 10)
 	frame:SetAllPoints(button)
 
-	animationGroup:Stop()
-	animationGroup:Play()
+	frame.button = button
 
-	animationNum = (animationNum % animationsCount) + 1
+	animationGroup:Play()
 end
 
-function KPA:LAB_OnButtonCreated(button)
-	button:HookScript("PreClick", animate)
+function KPA:GetFreeAnimationFrame()
+	return #framePool > 0 and tremove(framePool) or CreateAnimationFrame()
 end
 
 function KPA:UpdateSetting()
 	local db = E.db.enhanced.actionbar.keyPressAnimation
-	for i, v in ipairs(animations) do
-		local frame = v.frame
+
+	for _, frame in ipairs(self.frames) do
 		frame.texture:SetVertexColor(db.color.r, db.color.g, db.color.b)
 		frame.scale2:SetScale(db.scale, db.scale)
 		frame.rotation:SetDegrees(db.rotation)
@@ -82,12 +92,18 @@ end
 function KPA:Initialize()
 	if not E.private.enhanced.actionbar.keyPressAnimation then return end
 
-	self:UpdateSetting()
+	self.frames = {}
 
-	LAB.RegisterCallback(KPA, "OnButtonCreated", KPA.LAB_OnButtonCreated)
+	for i = 1, 3 do
+		tinsert(framePool, CreateAnimationFrame())
+	end
+
+	LAB.RegisterCallback(KPA, "OnButtonCreated", function(button)
+		button:HookScript("PreClick", StartAnimation)
+	end)
 
 	for button in pairs(LAB.buttonRegistry) do
-		button:HookScript("PreClick", animate)
+		button:HookScript("PreClick", StartAnimation)
 	end
 end
 
