dee97fe0ac13cea29f3adb88fd7772450948b975
Kader Bouyakoub <bkader@mail.com>
2021-06-24 03:16:55 +0100

    Update DPSLinks.lua

--- ElvUI_Enhanced/Modules/Chat/DPSLinks.lua
+++ ElvUI_Enhanced/Modules/Chat/DPSLinks.lua
@@ -46,7 +46,7 @@ local spamFirstLines = {
 local spamNextLines = {
 	"^(%d+)%. (.*)$",	-- Recount, Skada
 	"^ (%d+). (.*)$",	-- Skada
-	"^(.*) (.*)$",	-- Additional Skada
+	"^(.*): (.*)$", -- Additional Skada
 	"^.*%%%)$",			-- Skada player details
 	"^(%d+). (.*):(.*)(%d+)(.*)(%d+)%%(.*)%((%d+)%)$", -- TinyDPS
 }
